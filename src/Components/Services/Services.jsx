7bd9539d8b494514ccbd0a885a202ea3f8b200dd
ALNoman78 <abdullaalno078@gmail.com>
2025-10-21 21:20:17 +0600

    new services added

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -4,6 +4,7 @@ const Services = () => {
     return (
         <div>
             <h3 className='text-3xl font-black text-center py-10'>This is services page</h3>
+            <h3 className='text-3xl font-black text-center py-10'>This is services page</h3>
         </div>
     );
 };
